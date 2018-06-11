84e41f744fa3e08572c2033ce4444f2b8600c3dc
uca-alberto <castillo.aleman.alberto@gmail.com>
2018-06-11 12:54:43 -0600

    new files multiviews
    new added functionality, now you can see active and deleted clients through multiviews

--- CentroBiologiaMolecularUCA/CentroBiologiaMolecularUCA/Views/ViewCliente/BuscarCliente.aspx.designer.cs
+++ CentroBiologiaMolecularUCA/CentroBiologiaMolecularUCA/Views/ViewCliente/BuscarCliente.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace CentroBiologiaMolecularUCA.Views.ViewCliente {
     public partial class BuscarCliente {
         
         /// <summary>
+        /// Control ViewCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView ViewCliente;
+        
+        /// <summary>
+        /// Control ClientesActivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View ClientesActivos;
+        
+        /// <summary>
         /// Control form1.
         /// </summary>
         /// <remarks>
@@ -20,5 +38,41 @@ namespace CentroBiologiaMolecularUCA.Views.ViewCliente {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Control ClientesDesactivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View ClientesDesactivos;
+        
+        /// <summary>
+        /// Control form2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
+        
+        /// <summary>
+        /// Control Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
